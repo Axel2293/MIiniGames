c63ceaeaefe68bc5a8d8c438a14843e616c921a0
axel <axelwa@outlook.com>
2022-05-06 03:58:49 -0500

    Changes on menu and add PONG lvl 4

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 
 // This is for PONG game
-#define VERTICAL 25
+#define VERTICAL 18
 #define HORIZONTAL 80
 
 
@@ -40,9 +40,9 @@ void player_raquet(char [VERTICAL][HORIZONTAL], int, int);
 void ia_raquet(char [VERTICAL][HORIZONTAL], int, int);
 void print_pong(char [VERTICAL][HORIZONTAL], int,int);
 void ball(char[VERTICAL][HORIZONTAL], int, int);
-void pong_loop(char[VERTICAL][HORIZONTAL], int, int, int, int, int, int, int, int, int);
+void pong_loop(char[VERTICAL][HORIZONTAL], int, int, int, int, int, int, int, int, int, char);
 void draw(char [VERTICAL][HORIZONTAL], int, int);
-void input(char [VERTICAL][HORIZONTAL], int *,int *, int *, int *, int *, int *, int *, int *, int *, int *, int *);
+void input(char [VERTICAL][HORIZONTAL], int *,int *, int *, int *, int *, int *, int *, int *, int *, int *, int *, char);
 void update(char[VERTICAL][HORIZONTAL], int, int, int, int, int, int);
 void randomx(int *);
 
@@ -57,7 +57,7 @@ int main(){
     char qk_restart;
 
     switch(choice){
-        case 'p':
+        case '1':
             
             while(qk_restart!=27){
                 pong();
@@ -74,11 +74,11 @@ int main(){
 
 }
 
-
+// Main menu (shows available games)
 void print_menu(){
     system("cls");
     printf("\n\n\t===MINI GAMES===\n");
-    printf("\n \n ---PONG        == 'p'\n");
+    printf("\n \n ---PONG--- == 1 ==\n");
 
 }
 
@@ -90,11 +90,10 @@ int pong(){
 
     char field [VERTICAL][HORIZONTAL];
 
-
+    system("cls");
     printf("\n\n ===CONTROLLS===\n"
     "\n \tw -> UP\n\ts -> DOWN\n");
-    // printf("\n\t Start Game -> Enter\n\t Exit Game -> ESC\n\n");
-    printf("\nSelect dificulty...\n Easy --- 1\n Normal --- 2\n Hard --- 3\n.... ");
+    printf("\nSelect dificulty...\n Easy      --- 1\n Normal    --- 2\n Hard      --- 3\nImposible --- 4\n\n.... ");
     char key= getch();
 
     // Set the seed for the random numbers
@@ -104,37 +103,41 @@ int pong(){
     modx=-1;
     mody=-1;
     modia=-1;
+    randomx(&bally);
 
     switch (key){
         case '1':
-            randomx(&bally);
             inijug=8;
-            finjug=16;
+            finjug=14;
 
             iniia=8;
             finia=12;
             break;
         case '2':
-            randomx(&bally);
-            inijug=10;
+            inijug=9;
             finjug=14;
 
             iniia=6;
             finia=14;
             break;
         case '3':
-            randomx(&bally);
             inijug=10;
             finjug=14;
 
             iniia=3;
-            finia=20;
+            finia=15;
             break;
+        case '4':
+            inijug=8;
+            finjug=14;
+
+            iniia=8;
+            finia=12;
         default:
             break;
     }
     start(field, ballx, bally, inijug, finjug, iniia, finia);
-    pong_loop(field, ballx, bally, inijug, finjug, iniia, finia, modx, mody, modia);
+    pong_loop(field, ballx, bally, inijug, finjug, iniia, finia, modx, mody, modia, key);
     
 
     return 0;
@@ -154,6 +157,9 @@ void border_pong(char field[VERTICAL][HORIZONTAL]){
             }
             else if(j==0 || j==HORIZONTAL-1){
                 field[i][j]='#';
+            }
+            else if(j==(HORIZONTAL/2)){
+                field[i][j]='|';
             }
             else{
                 field[i][j]=' ';
@@ -175,7 +181,7 @@ void ia_raquet(char field[VERTICAL][HORIZONTAL], int iniia, int finia){
     }
 }
 void print_pong(char field[VERTICAL][HORIZONTAL], int score_player, int score_ia){
-    printf("Player %d             IA %d\n",score_player,score_ia );
+    printf("Player %d                                  IA %d\n",score_player,score_ia );
     for (int i=0; i<VERTICAL; i++){
         for(int j=0; j<HORIZONTAL; j++){
             printf("%c", field[i][j]);
@@ -187,13 +193,13 @@ void print_pong(char field[VERTICAL][HORIZONTAL], int score_player, int score_ia
 void ball(char field[VERTICAL][HORIZONTAL], int ballx, int bally){
     field[bally][ballx]='o';
 }
-void pong_loop(char field[VERTICAL][HORIZONTAL], int ballx,int bally, int inijug, int finjug, int iniia, int finia, int modx, int mody, int modia){
+void pong_loop(char field[VERTICAL][HORIZONTAL], int ballx,int bally, int inijug, int finjug, int iniia, int finia, int modx, int mody, int modia, char dif){
     int score_player=0, score_ia=0;
     do{
         draw(field, score_player, score_ia);
-        input(field, &ballx, &bally, &inijug, &finjug, &iniia, &finia, &modx, &mody, &modia, &score_player, &score_ia);
+        input(field, &ballx, &bally, &inijug, &finjug, &iniia, &finia, &modx, &mody, &modia, &score_player, &score_ia, dif);
         update(field, ballx, bally, inijug, finjug, iniia, finia);
-        // usleep(10);
+        // usleep(10000);
 
     }while (score_player <10 && score_ia<10);
     if (score_player==10){
@@ -211,7 +217,7 @@ void draw(char field[VERTICAL][HORIZONTAL], int score_player, int score_ia){
     print_pong(field, score_player, score_ia);
 }
 // Give all variables by reference, ant not by value so we can directly modify them
-void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug, int *finjug, int *iniia, int *finia, int *modx, int *mody, int *modia, int *score_player, int *score_ia){
+void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug, int *finjug, int *iniia, int *finia, int *modx, int *mody, int *modia, int *score_player, int *score_ia, char dif){
     
     char key;
 
@@ -226,6 +232,7 @@ void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug,
         randomx(bally);
         *ballx=40;
     }
+    // If ball hits right wall we count it as a point for the player
     if(*ballx==HORIZONTAL-2){
         *score_player+=1;
         randomx(bally);
@@ -237,6 +244,7 @@ void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug,
         for(int i=(*inijug); i<=(*finjug); i++ ){
             if(i == (*bally)){
                 *modx*=-1;
+                
             }
         }
     }
@@ -250,10 +258,23 @@ void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug,
         }
     }
 
+
     // Invert the direction of movement of the IA raquet once it reaches a border_pong
-    if(*iniia ==1 || *finia == VERTICAL-2){
-        *modia*=-1;
+    if (dif!='4'){
+        if(*iniia ==1 || *finia == VERTICAL-1){
+            *modia*=-1;
+        }
+    }else{
+        // Bit of more inteligence for the IA that makes the IA imposible to beat
+        if(*mody==-1){
+            *modia=-1;
+        }
+        else if(*mody==1){
+            *modia=1;
+        }
     }
+    
+    
 
     if (*score_ia<=10 && *score_player<=10){
     //BALL
@@ -261,9 +282,24 @@ void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug,
         *bally+=(*mody);
 
     //Raquet movement of IA
-        *iniia+=(*modia);
-        *finia+=(*modia);
-
+        if(dif=='4'){
+            if (*iniia ==1 && *modia==1){
+                *iniia+=(*modia);
+                *finia+=(*modia);
+            }
+            else if(*finia ==VERTICAL-1 && *modia==-1){
+                *iniia+=(*modia);
+                *finia+=(*modia);
+            }
+            else if(*iniia !=1 && *finia != VERTICAL-1){
+                *iniia+=(*modia);
+                *finia+=(*modia);
+            }
+        }
+        else{
+            *iniia+=(*modia);
+            *finia+=(*modia);
+        }
         //
         if (kbhit()==1){
             key= getch();
@@ -274,7 +310,7 @@ void input(char field[VERTICAL][HORIZONTAL], int *ballx,int *bally, int *inijug,
                 }
             }
             else if(key=='s'){
-                if(*finjug!=VERTICAL-2){
+                if(*finjug!=VERTICAL-1){
                     *finjug+=1;
                     *inijug+=1;
                 }
